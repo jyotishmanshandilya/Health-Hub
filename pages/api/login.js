99bce9fd02fcdae6e42daaa5127ba6d2403b4ac8
Adityashah2003 <adigshah14@gmail.com>
2023-11-01 18:31:21 +0530

    lol

---
+++ pages/api/login.js
@@ -0,0 +1,32 @@
+// pages/api/login.js
+
+import pool from '../../db';
+
+export default async (req, res) => {
+  if (req.method === 'POST') {
+    const { username, password } = req.body;
+
+    try {
+      const client = await pool.connect();
+      const result = await client.query(
+        'SELECT * FROM login WHERE username = $1 AND password = $2',
+        [username, password]
+      );
+
+      if (result.rows.length > 0) {
+        // Authentication successful
+        res.status(200).json({ message: 'Login successful' });
+      } else {
+        // Authentication failed
+        res.status(401).json({ message: 'Login failed' });
+      }
+
+      client.release();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  } else {
+    res.status(405).end(); // Method not allowed
+  }
+};
---
+++ pages/login.js
@@ -0,0 +1,61 @@
+// pages/login.js
+
+import { useState } from 'react';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        // Successful login
+        alert('Login successful');
+      } else {
+        // Failed login
+        alert('Login failed');
+      }
+    } catch (error) {
+      console.error(error);
+      alert('An error occurred during login.');
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Log In</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;

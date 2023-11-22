93045408b33a8b1febf87f25bd729fcfd340ee2e
Aditya <adigshah14@gmail.com>
2023-11-22 19:17:14 +0530

    added forget password

---
+++ app/api/auth/changepass/route.js
@@ -0,0 +1,38 @@
+import pool from '../../../../db';
+import { NextResponse } from 'next/server';
+
+export async function POST(req, res) {
+  const body = await req.json();
+  const { username, newPassword } = body;
+
+  try {
+    const client = await pool.connect();
+
+    // Update the password in the 'customer' table
+    let result = await client.query(
+      `UPDATE customer SET password = $1 WHERE username = $2`,
+      [newPassword, username]
+    );
+
+    if (result.rowCount === 0) {
+      // If no rows were updated in the 'customer' table, try updating the 'seller' table
+      result = await client.query(
+        `UPDATE seller SET password = $1 WHERE username = $2`,
+        [newPassword, username]
+      );
+    }
+
+    client.release();
+
+    if (result.rowCount > 0) {
+      // If a row was updated in either table, the password change was successful
+      return NextResponse.json({ message: 'Password change successful' }, { status: 200 });
+    } else {
+      // If no rows were updated in either table, the user does not exist
+      return NextResponse.json({ error: 'User not found' }, { status: 404 });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
+  }
+}
--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -2,44 +2,75 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
+// import { useState } from 'react';
+// import { useRouter } from 'next/router';
+
 const Login = () => {
   const router = useRouter();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [role, setRole] = useState('customer');
+  const [forgotPassword, setForgotPassword] = useState(false);
+  const [newPassword, setNewPassword] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      const response = await fetch('/api/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password, role }),
-      });
+    if (!forgotPassword) {
+      try {
+        const response = await fetch('/api/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password, role }),
+        });
 
-      if (response.ok) {
-        alert('Login successful');
-        if(role==='seller'){
-          router.push('/seller');
-        }else{
-        router.push('/');
+        if (response.ok) {
+          alert('Login successful');
+          if(role==='seller'){
+            router.push('/seller');
+          } else {
+            router.push('/');
+          }
+        } else if (response.status === 401) {
+          alert('Login failed...try again');
+        } else if (response.status === 401) {
+          alert('Role not recognized');
         }
-      } else if (response.status === 401) {
-        alert('Login failed...try again');
-      } else if (response.status === 401) {
-        alert('Role not recognized');
+      } catch (error) {
+        console.error(error);
+        alert('An error occurred during login.');
       }
-    } catch (error) {
-      console.error(error);
-      alert('An error occurred during login.');
+    } else {
+      try {
+        const response = await fetch('/api/auth/changepass', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, newPassword }),
+        });
+
+        if (response.ok) {
+          alert('Password change successful');
+          setForgotPassword(false);
+        } else {
+          alert('Failed to change password');
+        }
+      } catch (error) {
+        console.error(error);
+        alert('An error occurred while changing password');
+      }
     }
   };
 
+  const handleForgotPassword = () => {
+    setForgotPassword(true);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center" >
+    <div className="min-h-screen flex items-center justify-center">
       <div className="p-8 rounded shadow-lg w-full max-w-md bg-gray-100">
         <h1 className="text-2xl font-semibold mb-4">Login</h1>
         <form onSubmit={handleSubmit}>
@@ -49,6 +80,7 @@ const Login = () => {
               type="text"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
               className="border p-2 rounded w-full focus:outline-none focus:border-blue-500"
             />
           </div>
@@ -56,8 +88,9 @@ const Login = () => {
             <label className="block text-sm font-medium">Password:</label>
             <input
               type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={forgotPassword ? newPassword : password}
+              onChange={(e) => forgotPassword ? setNewPassword(e.target.value) : setPassword(e.target.value)}
+              placeholder={forgotPassword ? 'New Password' : 'Password'}
               className="border p-2 rounded w-full focus:outline-none focus:border-blue-500"
             />
           </div>
@@ -76,8 +109,16 @@ const Login = () => {
             type="submit"
             className="bg-blue-500 text-white p-2 rounded w-full hover:bg-blue-600 focus:outline-none focus:bg-blue-600"
           >
-            Log In
+            {forgotPassword ? 'Submit New Password' : 'Log In'}
           </button>
+          {!forgotPassword && (
+            <button
+              onClick={handleForgotPassword}
+              className="bg-gray-300 text-gray-700 p-2 rounded w-full mt-2 hover:bg-gray-400 focus:outline-none focus:bg-gray-400"
+            >
+              Forgot Password?
+            </button>
+          )}
         </form>
       </div>
     </div>
